dc4f25fd13bac18224f1c27c463f13502e7ef22c
zhenwu0831 <138546631+zhenwu0831@users.noreply.github.com>
2024-05-01 21:57:15 -0400

    Update plot.py

--- kilt/readers/t5/plot.py
+++ kilt/readers/t5/plot.py
@@ -2,54 +2,35 @@ import json
 import matplotlib.pyplot as plt
 import argparse
 
-PATH = "kilt/readers/t5/f1/"
+PATH = "kilt/readers/t5/all/"
 
-def plot_k_vs_macro_f1(file_path, dataset_name):
-    # Extract the dataset name from the filename
-    # dataset_name = file_path.split('/')[-1].split('_')[0]
+def map_metrics(dataset_name):
+    if dataset_name == 'fever' or dataset_name == 'trex' or dataset_name == 'zeroshot':
+        metric = 'accuracy'
+    elif dataset_name == 'nq' or dataset_name == 'hotpotqa' or dataset_name == 'triviaqa':
+        metric = 'em'
+    elif dataset_name == 'eli5':
+        metric = 'rougel'
+    elif dataset_name == 'wow':
+        metric = 'f1'
 
-    # Load the data from the JSON file
-    with open(file_path, 'r') as file:
-        data = json.load(file)
-    
-    # Prepare lists to store the values of k and corresponding Macro F1 scores
-    ks = []
-    macro_f1_scores = []
-    
-    # Iterate over possible k values, assuming they are labeled as 'k=0' to 'k=10'
-    for k in range(6):
-        key = f'k={k}'
-        if key in data:
-            ks.append(k)
-            macro_f1_scores.append(data[key]['macro_f1'])
-        else:
-            print(f"Warning: '{key}' not found in the data.")
-    
-    # Plotting the results
-    plt.figure(figsize=(10, 6))
-    plt.plot(ks, macro_f1_scores, marker='o', linestyle='-', color='b')
-    plt.title(f'{dataset_name}')
-    plt.xlabel('k')
-    plt.ylabel('Macro F1 Score')
-    plt.grid(True)
-    plt.xticks(ks)  # Ensure ticks for every k value present
-    # plt.show()
-    plt.savefig(f"{PATH}/{dataset_name}_f1.png")
+    return metric
 
-def plot_k_vs_macro_f1_multiple(fixed, M_top_k, M_adaptive_k, dataset):
+def plot_k_vs_macro_f1_multiple(fixed, M_adaptive_k, dataset, retriever):
     # Load the data from the JSON file
     with open(fixed, 'r') as file:
         fixed_data = json.load(file)
-    with open(M_top_k, 'r') as file:
-        M_top_k_data = json.load(file)
+    # with open(M_top_k, 'r') as file:
+    #     M_top_k_data = json.load(file)
     with open(M_adaptive_k, 'r') as file:
         M_adaptive_k_data = json.load(file)
+
+    metric = map_metrics(dataset)
     
     # Prepare lists to store the values of k and corresponding Macro F1 scores
     ks = []
-    fixed_macro_f1_scores = []
-    M_top_k_macro_f1_scores = []
-    M_adaptive_k_macro_f1_scores = []
+    fixed_metric_scores = []
+    M_adaptive_k_metric_scores = []
     
     # Iterate over possible k values, assuming they are labeled as 'k=0' to 'k=10'
     for k in range(6):
@@ -57,13 +38,11 @@ def plot_k_vs_macro_f1_multiple(fixed, M_top_k, M_adaptive_k, dataset):
         if key in fixed_data:
             ks.append(k)
             if k == 0:
-                M_top_k_macro_f1_scores.append(fixed_data[key]['macro_f1'])
-                M_adaptive_k_macro_f1_scores.append(fixed_data[key]['macro_f1'])
+                M_adaptive_k_metric_scores.append(fixed_data[key]['downstream'][metric])
             else:
-                M_top_k_macro_f1_scores.append(M_top_k_data[key]['macro_f1'])
-                M_adaptive_k_macro_f1_scores.append(M_adaptive_k_data[key]['macro_f1'])
+                M_adaptive_k_metric_scores.append(M_adaptive_k_data[key]['downstream'][metric])
 
-            fixed_macro_f1_scores.append(fixed_data[key]['macro_f1'])
+            fixed_metric_scores.append(fixed_data[key]['downstream'][metric])
             
         else:
             print(f"Warning: '{key}' not found in the data.")
@@ -76,17 +55,18 @@ def plot_k_vs_macro_f1_multiple(fixed, M_top_k, M_adaptive_k, dataset):
     
     # Plotting the results
     plt.figure(figsize=(10, 6))
-    plt.plot(ks, fixed_macro_f1_scores, marker='o', linestyle='-', color='blue', label='Fixed')
-    plt.plot(ks, M_top_k_macro_f1_scores, marker='o', linestyle='-', color='green', label='M_Top-k')
-    plt.plot(ks, M_adaptive_k_macro_f1_scores, marker='o', linestyle='-', color='orange', label='M_Adaptive-k')
-    plt.title(f'{dataset}')
+    plt.plot(ks, fixed_metric_scores, marker='o', linestyle='-', color='blue', label='Top-k')
+    plt.plot(ks, M_adaptive_k_metric_scores, marker='o', linestyle='-', color='orange', label='Adaptive-k')
+    plt.title(f'{dataset} with {retriever}')
     plt.xlabel('k')
-    plt.ylabel('Macro F1 Score')
+    if metric == 'em': 
+        plt.ylabel('Exact Match')
+    plt.ylabel(f'{metric.capitalize()}')
     plt.grid(True)
     plt.xticks(ks)  # Ensure ticks for every k value present
     plt.legend()
     # plt.show()
-    plt.savefig(f"{PATH}/{dataset}_f1.png")
+    plt.savefig(f"{PATH}/{dataset}_{metric}.png")
 
 
 if __name__ == "__main__":
@@ -94,11 +74,14 @@ if __name__ == "__main__":
     parser.add_argument(
         '--dataset', type=str, help='Name of the dataset'
     )
+    parser.add_argument(
+        '--retriever', type=str, help='Name of the retriever'
+    )
     args = parser.parse_args()
 
     fixed = f'{PATH}/fixed/{args.dataset}_results.json'
-    M_top_k = f'{PATH}/adaptive/{args.dataset}_results.json'
-    M_adaptive_k = f'{PATH}/adaptive_new/{args.dataset}_results.json'
+    # M_top_k = f'{PATH}/adaptive/{args.dataset}_results.json'
+    M_adaptive_k = f'{PATH}/adaptive/{args.dataset}_results.json'
 
     # plot_k_vs_macro_f1(f'{PATH}/{args.dataset}_results.json', args.dataset)
-    plot_k_vs_macro_f1_multiple(fixed, M_top_k, M_adaptive_k, args.dataset)
+    plot_k_vs_macro_f1_multiple(fixed, M_adaptive_k, args.dataset, args.retriever)
